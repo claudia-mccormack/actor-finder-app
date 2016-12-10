2c8cc570443ca7f7e470339f1a1c0375bd676067
Claudia McCormack <mccormack.chc@gmail.com>
2016-12-10 15:15:04 -0500

    search

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -11,11 +11,17 @@ require 'unirest'
   end
 
   def movies
-    @movies = Unirest.get('https://api.themoviedb.org/3/search/person?query=Brad+Pitt&api_key=54b0dadba188755475a177ad77b52815',
-                headers: {},).body
-    @titles = @movies["results"]
-    render json: => @titles
-    # @original_titles = []
-    # @titles.each {|key, value| @original_titles << value}
+    if params[:search]
+      @name_string = params[:search]
+      @name = @name_string.sub!(' ', '+')
+      @movies = Unirest.get("https://api.themoviedb.org/3/search/person?query=" + "#{@name}" + "&api_key=54b0dadba188755475a177ad77b52815").body
+      if @movies = []
+        @movies = Unirest.get("https://api.themoviedb.org/3/search/person?query=" + "Nicolas+Cage" + "&api_key=54b0dadba188755475a177ad77b52815").body
+      end
+      @titles = @movies["results"]
+      render json: @titles
+    else
+      render 'movies.html.erb'
+    end
   end
 end
