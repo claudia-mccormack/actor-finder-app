aa477afe16f10beb84618f48170638ea47cc5527
Claudia McCormack <mccormack.chc@gmail.com>
2016-12-10 15:33:37 -0500

    search fixed

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -15,11 +15,10 @@ require 'unirest'
       @name_string = params[:search]
       @name = @name_string.sub!(' ', '+')
       @movies = Unirest.get("https://api.themoviedb.org/3/search/person?query=" + "#{@name}" + "&api_key=54b0dadba188755475a177ad77b52815").body
-      if @movies = []
+      if @movies["total_results"] == 0
         @movies = Unirest.get("https://api.themoviedb.org/3/search/person?query=" + "Nicolas+Cage" + "&api_key=54b0dadba188755475a177ad77b52815").body
       end
-      @titles = @movies["results"]
-      render json: @titles
+      render json: @movies
     else
       render 'movies.html.erb'
     end
